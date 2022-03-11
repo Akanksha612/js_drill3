1f9694eb013c62ad97783dfd518494c073282fd8
Akanksha <akank612@gmail.com>
2022-03-11 18:30:13 +0530

    flatten added

---
+++ flatten.js
@@ -0,0 +1,12 @@
+
+
+    function flatten(nestedArray, depth) 
+    {
+        return depth > 0 ? nestedArray.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatten(val, depth- 1) : val), [])
+                     : nestedArray.slice();
+
+                     return nestedArray;
+     };
+
+
+     module.exports.flatten=flatten;
