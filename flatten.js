5ea1a70e6f5154c3e4d430a3f8438c85c522bdab
Akanksha <akank612@gmail.com>
2022-03-14 11:59:52 +0530

    flatten modified

--- flatten.js
+++ flatten.js
@@ -2,8 +2,7 @@
 
     function flatten(nestedArray, depth) 
     {
-        return depth > 0 ? nestedArray.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatten(val, depth- 1) : val), [])
-                     : nestedArray.slice();
+        return depth > 0 ? nestedArray.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatten(val, depth- 1) : val), []) : nestedArray.slice();
 
                      return nestedArray;
      };
